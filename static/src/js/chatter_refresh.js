fba6ca996cd2a1bd3903e79eadcee8442a183b64
yousifmoufti996 <147701634+yousifmoufti996@users.noreply.github.com>
2024-10-20 20:25:15 +0300

    update js

--- static/src/js/chatter_refresh.js
+++ static/src/js/chatter_refresh.js
@@ -1,20 +1,32 @@
 /** @odoo-module **/
 
 import { FormController } from "@web/views/form/form_controller";
-import { formView } from "@web/views/form/form_view";
 import { patch } from "@web/core/utils/patch";
-import { useEffect } from "@odoo/owl";
+import { useEffect, onWillStart } from "@odoo/owl";
 
 patch(FormController.prototype, {
     setup() {
-        const originalSetup = FormController.prototype.setup;
-        originalSetup.call(this, ...arguments);
+        const superSetup = this._super;
+        if (superSetup) {
+            superSetup.call(this, ...arguments);
+        }
         
+        onWillStart(() => {
+            this.chatterRefreshSetup();
+        });
+    },
+
+    chatterRefreshSetup() {
         useEffect(() => {
-            const chatter = this.el.querySelector('.o_Chatter');
+            const chatter = this.el && this.el.querySelector('.o_Chatter');
             if (chatter) {
                 chatter.addEventListener('click', this.onSendButtonClick.bind(this));
             }
+            return () => {
+                if (chatter) {
+                    chatter.removeEventListener('click', this.onSendButtonClick.bind(this));
+                }
+            };
         });
     },
 
@@ -22,8 +34,10 @@ patch(FormController.prototype, {
         if (event.target.closest('.o_Composer_buttonSend')) {
             // Wait for the message to be sent
             setTimeout(() => {
-                this.model.root.load();
-                const chatter = this.el.querySelector('.o_Chatter');
+                if (this.model && this.model.root) {
+                    this.model.root.load();
+                }
+                const chatter = this.el && this.el.querySelector('.o_Chatter');
                 if (chatter) {
                     const messageList = chatter.querySelector('.o_Chatter_scrollPanel');
                     if (messageList) {
@@ -34,5 +48,3 @@ patch(FormController.prototype, {
         }
     },
 });
-
-// No need to patch the formView here, as we're directly patching the FormController prototype
