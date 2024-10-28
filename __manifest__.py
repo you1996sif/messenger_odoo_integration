61724a65009978a123ed1f083cf099d7aafb6cab
yousifmoufti996 <147701634+yousifmoufti996@users.noreply.github.com>
2024-10-29 01:42:12 +0300

    ff

--- __manifest__.py
+++ __manifest__.py
@@ -34,7 +34,7 @@
         'views/privacy_policy_templates.xml',
         'views/facebook_conversation_views.xml',
         'views/facebook_user_conversation_views.xml',
-        'views/helpdesk_ticket_views.xml',
+        # 'views/helpdesk_ticket_views.xml',
         'wizards/create_sale_order_wizard_views.xml',
         
 
