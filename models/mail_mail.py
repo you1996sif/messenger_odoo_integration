11651b904ddb04f1a4422d23eb3f50ffa043f740
yousifmoufti996 <147701634+yousifmoufti996@users.noreply.github.com>
2024-11-02 20:11:56 +0300

    Revert "Update mail_mail.py"
    This reverts commit 67002fac655a0f98fa893fe4ce0011ccd5aa85a5.

--- models/mail_mail.py
+++ models/mail_mail.py
@@ -5,14 +5,9 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+
 class MailMail(models.Model):
     _inherit = 'mail.mail'
-
-    is_facebook_message = fields.Boolean(
-        string='Is Facebook Message',
-        help='Technical field to identify messages meant for Facebook',
-        default=False
-    )
 
     @api.model
     def create(self, values):
@@ -22,55 +17,26 @@ class MailMail(models.Model):
                 if isinstance(headers, dict) and 'X-Odoo-Objects' in headers:
                     x_odoo_objects = headers['X-Odoo-Objects']
                     if isinstance(x_odoo_objects, str) and x_odoo_objects.startswith('facebook.user.conversation'):
-                        values['is_facebook_message'] = True
-                        # Create the record but don't set state to sent yet
-                        # It will be set to sent after successful Facebook sending
-                        _logger.info("Created facebook conversation message record")
-            except (ValueError, SyntaxError) as e:
-                _logger.error("Error parsing headers: %s", str(e))
+                        # Set the state to 'sent' immediately
+                        values['state'] = 'sent'
+                        # Optionally, set a sent date
+                        values['date'] = fields.Datetime.now()
+            except (ValueError, SyntaxError):
+                # If there's an error parsing the headers, just proceed normally
                 pass
 
-        return super().create(values)
+        return super(MailMail, self).create(values)
 
     def _send(self, auto_commit=False, raise_exception=False, smtp_session=None):
-        # Split records between Facebook messages and regular emails
-        facebook_messages = self.filtered(lambda m: m.is_facebook_message)
-        regular_emails = self - facebook_messages
+        # For emails related to facebook.user.conversation, mark as sent without actually sending
+        facebook_mails = self.filtered(lambda m: m.model == 'facebook.user.conversation')
+        if facebook_mails:
+            facebook_mails.write({'state': 'sent'})
         
-        # Process Facebook messages
-        for message in facebook_messages:
-            try:
-                # Get the related Facebook conversation
-                conversation = self.env['facebook.user.conversation'].browse(message.res_id)
-                if conversation:
-                    # Here you would call your method to send to Facebook
-                    # This is a placeholder - replace with your actual Facebook sending logic
-                    self.env['facebook.user.conversation'].send_facebook_message(
-                        conversation_id=conversation.id,
-                        message_body=message.body_html,  # or message.body if you prefer plain text
-                        attachment_ids=message.attachment_ids,
-                    )
-                    
-                    # Mark as sent after successful Facebook sending
-                    message.write({
-                        'state': 'sent',
-                        'date': fields.Datetime.now()
-                    })
-                    _logger.info(f"Successfully sent Facebook message for conversation {conversation.id}")
-            except Exception as e:
-                _logger.error(f"Error sending Facebook message: {str(e)}")
-                if raise_exception:
-                    raise
+        # Process other emails normally
+        return super(MailMail, self - facebook_mails)._send(
+            auto_commit=auto_commit, raise_exception=raise_exception, smtp_session=smtp_session)
         
-        # Process regular emails normally
-        if regular_emails:
-            return super(MailMail, regular_emails)._send(
-                auto_commit=auto_commit,
-                raise_exception=raise_exception,
-                smtp_session=smtp_session
-            )
-        return True
-
     @api.model
     def search_and_cancel_by_body(self, body_text):
         if not body_text:
