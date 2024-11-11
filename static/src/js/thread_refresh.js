98ea72f297e9d5fd94c6fb7766cb53b5311873a5
yousifmoufti996 <147701634+yousifmoufti996@users.noreply.github.com>
2024-11-11 23:42:13 +0300

    sds

--- static/src/js/thread_refresh.js
+++ static/src/js/thread_refresh.js
@@ -1,34 +1,39 @@
-/** @odoo-module */
+/** @odoo-module **/
 
 import { ThreadField } from "@mail/core/common/thread_field";
 import { patch } from "@web/core/utils/patch";
 
-patch(ThreadField.prototype, {
+patch(ThreadField.prototype, 'messenger_integration/static/src/js/thread_refresh.js', {
     setup() {
-        super.setup();
-        this.startAutoRefresh();
+        this._super(...arguments);
+        this.setupAutoRefresh();
     },
 
-    startAutoRefresh() {
+    setupAutoRefresh() {
+        if (this.props.record.resModel === 'facebook.user.conversation') {
+            this.startRefreshInterval();
+        }
+    },
+
+    startRefreshInterval() {
         if (this.refreshInterval) {
             clearInterval(this.refreshInterval);
         }
-        
-        // Set up auto-refresh every second
         this.refreshInterval = setInterval(() => {
-            this.reloadThread();
+            this.reloadMessages();
         }, 1000);
     },
 
-    destroy() {
+    async reloadMessages() {
+        if (this.threadService) {
+            await this.threadService.fetchMessages();
+        }
+    },
+
+    onWillDestroy() {
         if (this.refreshInterval) {
             clearInterval(this.refreshInterval);
         }
-        super.destroy();
+        this._super(...arguments);
     },
-
-    reloadThread() {
-        // Trigger thread reload
-        this.props.reloadCallback?.();
-    }
 });
