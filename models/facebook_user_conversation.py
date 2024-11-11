1c203b2cb72532470bdd2d280ad243dd34593bea
yousifmoufti996 <147701634+yousifmoufti996@users.noreply.github.com>
2024-11-11 22:14:07 +0300

    Update facebook_user_conversation.py

--- models/facebook_user_conversation.py
+++ models/facebook_user_conversation.py
@@ -210,7 +210,7 @@ class FacebookUserConversation(models.Model):
                     raise UserError(_("Failed to send message to Facebook."))
                     
                 # Create a record in facebook_conversation
-                conv = self.env['facebook_conversation'].sudo().create({
+                self.env['facebook_conversation'].sudo().create({
                     'user_conversation_id': self.id,
                     'partner_id': self.partner_id.id,
                     'message': clean_body,
@@ -219,7 +219,7 @@ class FacebookUserConversation(models.Model):
                     'message_type': 'comment'
                 })
                 _logger.info("elf.env['facebook_conversation'].sudo().create({")
-                conv.add_message_to_chatter(message, 'odoo')
+                self.add_message_to_chatter(message, 'odoo')
                 
                 
                 return message
