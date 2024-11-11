fa632bd380fb006576ec0fa78db6f1fa33e3ad16
yousifmoufti996 <147701634+yousifmoufti996@users.noreply.github.com>
2024-11-11 23:11:06 +0300

    UPDATE

--- __init__.py
+++ __init__.py
@@ -6,14 +6,3 @@ from . import wizards
 
 
 
-# from . import utils
-
-# messenger_integration/models/__init__.py
-from . import facebook_user_conversation
-from . import FacebookConversation
-
-# messenger_integration/controllers/__init__.py
-from . import nnewmessenger
-
-# messenger_integration/utils/__init__.py
-from . import facebook_utils
