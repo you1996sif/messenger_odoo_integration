8f3a636d5df28f2b4fd091af9cbac250bdddc30f
yousifmoufti996 <147701634+yousifmoufti996@users.noreply.github.com>
2024-11-11 22:20:44 +0300

    Update mail_mail.py

--- models/mail_mail.py
+++ models/mail_mail.py
@@ -96,20 +96,20 @@ class MailMail(models.Model):
 
         return super(MailMail, self).create(values)
 
-    def _send(self, auto_commit=False, raise_exception=False, smtp_session=None):
-        _logger.info(' def _send(self, auto_comm')
-        # For emails related to facebook.user.conversation, mark as sent without actually sending
-        facebook_mails = self.filtered(lambda m: m.model == 'facebook.user.conversation')
-        _logger.info(' facebook_mails')
-        _logger.info(facebook_mails)
-        if facebook_mails:
-            _logger.info('ifffffacebook_mails')
-            _logger.info(facebook_mails)
-            facebook_mails.write({'state': 'sent'})
+    # def _send(self, auto_commit=False, raise_exception=False, smtp_session=None):
+    #     _logger.info(' def _send(self, auto_comm')
+    #     # For emails related to facebook.user.conversation, mark as sent without actually sending
+    #     facebook_mails = self.filtered(lambda m: m.model == 'facebook.user.conversation')
+    #     _logger.info(' facebook_mails')
+    #     _logger.info(facebook_mails)
+    #     if facebook_mails:
+    #         _logger.info('ifffffacebook_mails')
+    #         _logger.info(facebook_mails)
+    #         facebook_mails.write({'state': 'sent'})
         
-        # Process other emails normally
-        return super(MailMail, self - facebook_mails)._send(
-            auto_commit=auto_commit, raise_exception=raise_exception, smtp_session=smtp_session)
+    #     # Process other emails normally
+    #     return super(MailMail, self - facebook_mails)._send(
+    #         auto_commit=auto_commit, raise_exception=raise_exception, smtp_session=smtp_session)
         
     @api.model
     def search_and_cancel_by_body(self, body_text):
@@ -118,6 +118,8 @@ class MailMail(models.Model):
             _logger.info('if not body_text')
             return
         else:
+            
+            _logger.info('else')
             return {
         'type': 'ir.actions.client',
         'tag': 'reload'}
