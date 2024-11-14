0c5eb20b67dc188e9c23b8565f580df3bd668a66
yousifmoufti996 <147701634+yousifmoufti996@users.noreply.github.com>
2024-11-14 16:18:06 +0300

    Update helpdesk_ticket_inherit.py

--- models/helpdesk_ticket_inherit.py
+++ models/helpdesk_ticket_inherit.py
@@ -1,11 +1,27 @@
 # Create a new file named helpdesk_ticket_inherit.py
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     facebook_conversation_id = fields.Many2one('facebook.user.conversation', string='Facebook Conversation')
+    
+    
+    
+    
+    @api.model
+    def create(self, vals):
+        record = super(HelpdeskTicket, self).create(vals)
+        if vals.get('user_id'):
+            record.message_follower_ids.unlink()
+        return record
+    
+    def write(self, vals):
+        result = super(HelpdeskTicket, self).write(vals)
+        if vals.get('user_id'):
+            self.message_follower_ids.unlink()
+        return result
     
     def action_view_facebook_conversation(self):
         self.ensure_one()
